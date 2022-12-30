876610e8954c79b09445e50962e37c996ad829df
yarulliin <rifatyarullin@MacBook-Air-Rifat.local>
2022-12-30 15:01:02 +0400

    fix prefix

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-layout',
+  selector: 'yf-layout',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './layout.component.html',
