dd41a9aac4d5eef18e5d4c7dcc348aaba9e5e3fd
yarulliin <rifatyarullin@MacBook-Air-Rifat.local>
2022-12-30 13:43:46 +0400

    Revert "create standalone component"
    This reverts commit 5a7a9b060e101c1ed301cf8ddcd75b5d04b8d599.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  standalone: true,
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = 'yard-food-front-end';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent);
+import { AppModule } from './app/app.module';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
